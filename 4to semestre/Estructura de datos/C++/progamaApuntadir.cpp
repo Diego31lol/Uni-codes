72a6a865d6a3354dd20447bd51df4522485e9f07
Diego31lol <diego31lol@gmail.com>
2025-02-17 11:23:11 -0600

    a

--- 4to semestre/Estructura de datos/C++/progamaApuntadir.cpp
+++ 4to semestre/Estructura de datos/C++/progamaApuntadir.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 int main(){
-    system(cls);
+ //   system(cls);
 int num1=10, *aptNum1, apt2Num, x;
 
 
@@ -13,8 +13,8 @@ cout << "?nla direccion de memoria de num2 es:"<<aptNum1;
 
 
 
-apt2Num=aptNum1;
-x=*apt2Num +10;
+//apt2Num=aptNum1;
+//x=*apt2Num +10;
 cout<<x;
 cout <<endl <<endl;
 

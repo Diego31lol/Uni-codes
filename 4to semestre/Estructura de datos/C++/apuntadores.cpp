8a61e2053b2520f563f5af3e506478083c4bb496
Diego31lol <diego31lol@gmail.com>
2025-02-17 11:42:35 -0600

    ss
    s

--- 4to semestre/Estructura de datos/C++/apuntadores.cpp
+++ 4to semestre/Estructura de datos/C++/apuntadores.cpp
@@ -1,11 +1,26 @@
-include <cstdlib>
+#include <iostream> // Se incluye la librería correcta
 
 using namespace std;
 
-int main(){
+int main() {
+    // Definición del arreglo con valores correctos
+    int ArrNum[10] = {10, 20, 25, 30, 35, 40, 0, 0, 0, 0}; 
 
-    int ArrNum[10]=(10,20,25,30,35,40);
+    // Definición del puntero apuntando al primer elemento del arreglo
+    int* aptArrNum = ArrNum;
 
-cout <<"hola";
+    // Imprimir mensaje de prueba
+    cout << "Hola" << endl;
 
+    // Acceder a los elementos del arreglo mediante el índice
+    cout << "El elemento en la posicion [0] es: " << ArrNum[0] << endl;
+    cout << "El elemento en la posicion [0] es: " << aptArrNum[0] << endl;
+
+    cout << "El elemento en la posicion [2] es: " << ArrNum[2] << endl;
+    cout << "El elemento en la posicion [2] es: " << aptArrNum[2] << endl;
+
+    // Mostrar la dirección de memoria del elemento en la posición [2]
+    cout << "La direccion de memoria de la posicion [2] es: " << &ArrNum[2] << endl;
+    cout << "La direccion de memoria de la posicion [2] es: " << &aptArrNum[2] << endl;
+    return 0; // Retorno exitoso del programa
 }
